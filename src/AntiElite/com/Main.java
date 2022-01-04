4559278d7132defd0823678ecb73646ce9c91ac6
Nametsego <Nametsegothakadu@gmail.com>
2022-01-04 06:02:57 +0200

    Initial Commit

---
+++ src/AntiElite/com/Main.java
@@ -0,0 +1,10 @@
+package AntiElite.com;
+
+import javax.swing.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+        JOptionPane.showMessageDialog(null,"Call tech support NOW OR ELSE !!!!","Error", JOptionPane.ERROR_MESSAGE);
+    }
+}
